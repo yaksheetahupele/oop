df125f176bdc6b7141d6e38cc24a4e45d4d3e6a2
yaksheetahupele <yaksheetahupele@gmail.com>
2026-08-03 22:19:44 +0530

    change

---
+++ arithmetic operators.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cout << "enter two numbers";
+    cin >> a >> b
+                        cout
+                    << "addition:" << a + b << endl;
+    cout << "Addition = " << a + b << endl;
+    cout << "Subtraction = " << a - b << endl;
+    cout << "Multiplication = " << a * b << endl;
+    cout << "Division = " << a / b << endl;
+    cout << "Modulus = " << a % b << endl;
+    return 0;
+}
