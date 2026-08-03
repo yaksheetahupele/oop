07be6b0fc1d011dbb48284bc545328503ece3a2b
yaksheetahupele <yaksheetahupele@gmail.com>
2026-08-03 22:27:51 +0530

    Update arithmetic_operators.cpp

--- arithmetic_operators.cpp
+++ arithmetic_operators.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
     int a, b;
-    cout << "enter two numbers";
-    cin >> a >> b
-                        cout
-                    << "addition:" << a + b << endl;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
     cout << "Addition = " << a + b << endl;
     cout << "Subtraction = " << a - b << endl;
     cout << "Multiplication = " << a * b << endl;
     cout << "Division = " << a / b << endl;
     cout << "Modulus = " << a % b << endl;
+
     return 0;
 }
