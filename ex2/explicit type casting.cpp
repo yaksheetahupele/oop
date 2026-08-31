1821591b42007e8275b81532a2504d8b65a66519
yaksheetahupele <yaksheetahupele@gmail.com>
2026-08-31 21:36:57 +0530

    8

---
+++ ex2/explicit type casting.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a = 10.75;
+    int b;
+
+    b = (int)a; // Explicit type casting
+
+    cout << "Float value = " << a << endl;
+    cout << "Integer value = " << b << endl;
+
+    return 0;
+}
